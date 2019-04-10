4767e7fd67da1c41ba0160f8d566041c4df18b56
Fang Xu <fangxu99@yahoo.com>
2019-04-10 13:24:09 -0400

    event binding

--- my-angular-albums/src/app/app.component.ts
+++ my-angular-albums/src/app/app.component.ts
@@ -9,21 +9,6 @@ import { Album } from './albums/album.model';
 })
 export class AppComponent implements OnInit {
     title = 'my-angular-albums';
-    albumsArray: Album[];
-    titleCounter = 1;
-    numbers = [1, 2, 3];
 
-    ngOnInit(): void {
-        // const interval = setInterval(() => this.titleCounter++, 2000);
-        const interval = setInterval(() => {
-            this.titleCounter++;
-            this.numbers.push(4);
-            this.numbers = [...this.numbers];
-
-            this.albumsArray[0].price += 10;
-
-            console.log(this.numbers);
-        }, 2000);
-        setTimeout(() => clearInterval(interval), 6000);
-    }
+    ngOnInit(): void {}
 }
