6ef791afbb647e1c582cfa832ba487841a2d27b3
Fang Xu <fangxu99@yahoo.com>
2019-04-11 09:53:40 -0400

    pipe

--- my-angular-albums/src/app/albums/album-card/album-card.component.ts
+++ my-angular-albums/src/app/albums/album-card/album-card.component.ts
@@ -13,12 +13,20 @@ export class AlbumCardComponent implements OnInit {
 
     @Output()
     albumClicked: EventEmitter<Album> = new EventEmitter<Album>();
+
+    newPrice: number;
+
     constructor() {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        if (this.album.on_sale) {
+            // Apply 10% discount
+            this.newPrice = this.album.price - this.album.price * 0.1;
+        }
+    }
 
     showAlbum() {
-        //alert('Album selected: ' + this.album.album_name);
+        // alert('Album selected: ' + this.album.album_name);
         this.albumClicked.emit(this.album);
     }
 }
